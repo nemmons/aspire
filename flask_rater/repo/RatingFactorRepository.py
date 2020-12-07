ca6fa67bc1092b81c0d04f2d270aaa3cd3f39726
Nate Emmons <nateemmons@gmail.com>
2020-12-07 16:27:43 -0500

    Add RatingFactorRepository for db lookups

--- flask_rater/repo/RatingFactorRepository.py
+++ flask_rater/repo/RatingFactorRepository.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+from sqlalchemy import asc, desc
+from sqlalchemy.orm import session
+from models import RatingFactor as RatingFactorModel
 
 
 class AbstractRatingFactorRepository(ABC):
@@ -12,3 +15,39 @@ class AbstractRatingFactorRepository(ABC):
     @abstractmethod
     def get_factor(self, rating_factor_type: str, params: dict, options=None):
         pass
+
+
+class RatingFactorRepository(AbstractRatingFactorRepository):
+    def __init__(self, rating_manual_id, db_session: session):
+        super().__init__()
+        self.rating_manual_id = rating_manual_id
+        self.db_session = db_session
+
+    def lookup(self, rating_factor_type: str, params: dict, options: dict = None):
+        result = self.get_factor(rating_factor_type, params, options)
+        return result.value
+
+    def get_factor(self, rating_factor_type: str, params: dict, options: dict = None):
+        options = {} if not options else options
+
+        query = self.db_session.query(RatingFactorModel) \
+            .filter(RatingFactorModel.rating_manual_id == self.rating_manual_id) \
+            .filter(RatingFactorModel.type == rating_factor_type)
+
+        if "step_up" in options.keys():
+            col_to_step = options['step_up']
+            target = params[col_to_step]
+            params.pop(col_to_step)
+
+            query = query.filter(getattr(RatingFactorModel, col_to_step) >= target) \
+                .order_by(asc(col_to_step))
+        elif "step_down" in options.keys():
+            col_to_step = options['step_down']
+            target = params[col_to_step]
+            params.pop(col_to_step)
+
+            query = query.filter(getattr(RatingFactorModel, col_to_step) <= target) \
+                .order_by(desc(col_to_step))
+
+        result = query.filter_by(**params).first()
+        return result  # Todo hydrate into something other than a RatingFactorModel - this should be a domain entity
